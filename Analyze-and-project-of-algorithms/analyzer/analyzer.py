2094314a34b4f887ffeaadd13534620e33149493
Wesley <84673115+iW3ll@users.noreply.github.com>
2024-10-08 15:59:57 -0300

    analyzer update

--- Analyze-and-project-of-algorithms/analyzer/analyzer.py
+++ Analyze-and-project-of-algorithms/analyzer/analyzer.py
@@ -107,7 +107,7 @@ def analyze_code(file_path, file_type):
     if recursion_count > 0:
         print(f"- Pior caso: O código contem {recursion_count} recursão(ões). Complexidade depende da função recursiva (O(T(n))).")
     if is_greedy:
-        print("- Algoritmo potencialmente guloso: Baseado em decisões locais detectadas (ex., uso de max, min, etc.).")
+        print("- Algoritmo potencialmente guloso: Baseado em decisões locais detectadas
     else:
         print("- Nenhum algoritmo guloso detectado.")
 
